58ee1ae0bca3488c7504de11ee844d811507910c
Nicolas Troplent <nicolas.troplent@gmail.com>
2022-11-05 11:17:00 -0400

    turn back test label off

--- api/routes/createLabel.js
+++ api/routes/createLabel.js
@@ -12,10 +12,10 @@ const requestPrep = require("../middlewares/requestPrep");
 const handleRequest = require("../middlewares/handleRequest");
 const storeDB = require("../middlewares/storeDB");
 const responseObject = require("../middlewares/responseObject");
-const validateShippingService2 = require("../middlewares/validateShipping2.0");
-const requestPrep2 = require("../middlewares/requestPrep2.0");
-const handleRequest2 = require("../middlewares/handleRequest2.0");
-const storeDB2 = require("../middlewares/storeDB2.0");
+// const validateShippingService2 = require("../middlewares/validateShipping2.0");
+// const requestPrep2 = require("../middlewares/requestPrep2.0");
+// const handleRequest2 = require("../middlewares/handleRequest2.0");
+// const storeDB2 = require("../middlewares/storeDB2.0");
 
 router.post(
   "/webhooks/print-label/:alias",
@@ -33,21 +33,21 @@ router.post(
   }
 );
 
-router.post(
-  "/webhooks/generate-label/:alias",
-  validateWebhook,
-  checkPackageInput,
-  validateAddress,
-  validateShippingService2,
-  requestPrep2,
-  // handleRequest2,
-  // storeDB2,
-  // responseObject,
-  async (req, res) => {
-    const webhookResponse = req.webhookResponse;
-    res.status(200).json(webhookResponse);
-    // res.status(200).json(req.requestBody);
-  }
-);
+// router.post(
+//   "/webhooks/generate-label/:alias",
+//   validateWebhook,
+//   checkPackageInput,
+//   validateAddress,
+//   validateShippingService2,
+//   requestPrep2,
+//   // handleRequest2,
+//   // storeDB2,
+//   // responseObject,
+//   async (req, res) => {
+//     const webhookResponse = req.webhookResponse;
+//     res.status(200).json(webhookResponse);
+//     // res.status(200).json(req.requestBody);
+//   }
+// );
 
 module.exports = router;
